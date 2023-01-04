8ff2944f1cc6bf3d5a48986ee17c0c5b406cfe65
wook <leeyong1321@gmail.com>
2023-01-05 01:17:14 +0900

    2_변수 이름 바꾸기

--- src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_02_rename_variable/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_02_rename_variable/StudyDashboard.java
@@ -25,10 +25,10 @@ public class StudyDashboard {
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
         GHIssue issue = repository.getIssue(30);
 
-        List<GHIssueComment> comments = issue.getComments();
-        for (GHIssueComment comment : comments) {
-            usernames.add(comment.getUserName());
-            this.reviews.add(comment.getBody());
+        List<GHIssueComment> reviews = issue.getComments();
+        for (GHIssueComment review : reviews) {
+            usernames.add(review.getUserName());
+            this.reviews.add(review.getBody());
         }
     }
 
@@ -43,7 +43,7 @@ public class StudyDashboard {
     public static void main(String[] args) throws IOException {
         StudyDashboard studyDashboard = new StudyDashboard();
         studyDashboard.loadReviews();
-        studyDashboard.getUsernames().forEach(name -> System.out.println(name));
-        studyDashboard.getReviews().forEach(review -> System.out.println(review));
+        studyDashboard.getUsernames().forEach(System.out::println);
+        studyDashboard.getReviews().forEach(System.out::println);
     }
 }
